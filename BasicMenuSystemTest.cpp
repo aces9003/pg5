6717f6d7bddb51567b49f78d0503c58a6f1b95bd
aces9003 <aces_9003@yahoo.com>
2014-10-27 16:03:12 -0400

    Updated Menu System
    - Includes goto statements
    - Some formatting changes in the outoput

--- BasicMenuSystemTest.cpp
+++ BasicMenuSystemTest.cpp
@@ -9,20 +9,22 @@ int main() {
 	
 	clearScreen();
 	
-	while(true) {
-		char mainMenuChoice;
-		// char name[x];
-		
-		cout << "Main Menu::PhoneList\n\n"
-			 << " 1) Edit Contacts List\n"
-			 << " 2) Edit Favorites List\n"
-			 << " 3) Display All Contacts\n"
-			 << " 4) Display Favorites\n"
-			 << " 5) Exit PhoneList\n\n"
-			 << "Selection: ";
-		cin >> mainMenuChoice;
-		clearScreen();
-
+    startMainMenu:
+    while(true) {
+        char mainMenuChoice;
+        // char name[x];
+        
+        cout << "::Main Menu::PhoneList\n\n"
+             << " 1) Edit Contacts List\n"
+             << " 2) Edit Favorites List\n"
+             << " 3) Display All Contacts\n"
+             << " 4) Display Favorites\n"
+             << " 5) Exit PhoneList\n\n"
+             << "Selection: ";
+        cin >> mainMenuChoice;
+        clearScreen();
+        
+        startSwitch:
         switch (mainMenuChoice) {
             case '1':
                 // First display contacts, names and indexes only
@@ -33,18 +35,23 @@ int main() {
                 clearScreen();
                 
                 char editFavsList;
-                cout << "**All favorites + indexes (no number pairs)**\n\n"
-                << " 1) Add Contact to Favorites\n"
-                << " 2) Delete Contact from Favorites\n"
-                << " 3) Rearrange Favorites\n"
-                << " 4) Exit PhoneList\n\n"
-                << "Selection: ";
+                cout << "**All favorites + indexes (no number pairs)**\n"
+                     << "=============================================\n\n"
+                     << "::Edit Favorites List::\n\n"
+                     << " 1) Add Contact to Favorites\n"
+                     << " 2) Delete Contact from Favorites\n"
+                     << " 3) Rearrange Favorites\n"
+                     << " 4) Return to Main Menu\n\n"
+                     << "Selection: ";
                 cin >> editFavsList;
                 cout << endl;
                 
                 if (editFavsList > '4' || editFavsList < '1') {
                     cout << "Invalid Input Entered\n\n";
-                    system( "read -n 1 -s -p \"Press any key to return to the main menu...\"" );
+                    system( "read -n 1 -s -p \"Press any key to return to the previous menu...\"" );
+                    mainMenuChoice = '2';
+                    clearScreen();
+                    goto startSwitch;
                 } else if (editFavsList == '1') {
                     // Add contact from full list to favorites
                     // Print full contacts, names and indexes only
@@ -74,34 +81,36 @@ int main() {
                     clearScreen();
                 } else if (editFavsList == '4') {
                     clearScreen();
-                    cout << "Thanks for using PhoneList.\n\n";
-                    mainMenuChoice = '5';
+                    goto startMainMenu;
                 } else {
                     cout << "Invalid Input Entered\n\n";
-                    system( "read -n 1 -s -p \"Press any key to return to the main menu...\"" );
-                    mainMenuChoice = '5';
+                    system( "read -n 1 -s -p \"Press any key to return to the previous menu...\"" );
+                    mainMenuChoice = '2';
                     clearScreen();
+                    goto startSwitch;
                 }
                 break;
                 
             case '3':
                 // Display all contacts with number pairs
                 clearScreen();
-                cout << "**All contacts + number pairs go here**\n\n";
+                cout << "**All contacts + number pairs go here**\n"
+                << "=============================================\n\n";
                 // displayAll();
-                system( "read -n 1 -s -p \"Press any key to return to Main Menu...\"" );
+                system( "read -n 1 -s -p \"Press any key to return to the main menu...\"" );
                 clearScreen();
                 break;
-            
+                
             case '4':
                 // Display all favorites with number pairs
                 clearScreen();
-                cout << "**All favorites + number pairs go here**\n\n";
+                cout << "**All favorites + number pairs go here**\n"
+                << "=============================================\n\n";
                 // dispalyFavs();
-                system( "read -n 1 -s -p \"Press any key to return to Main Menu...\"" );
+                system( "read -n 1 -s -p \"Press any key to return to the main menu...\"" );
                 clearScreen();
                 break;
-            
+                
             case '5':
                 clearScreen();
                 cout << "Thanks for using PhoneList.\n\n";
